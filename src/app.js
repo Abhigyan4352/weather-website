24e7302d0d67ffa0fda0ab273e621c62d7769b48
Abhigyan Utsav <f20180194@goa.bits-pilani.ac.in>
2020-07-17 10:15:12 +0530

    name changed

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
+        title: 'Forecast',
         name: 'Abhigyan Utsav'
     })
 })
